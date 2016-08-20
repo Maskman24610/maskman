fed74fc0f472505d42f5d34aad1e0fd791b49e8b
Hsu yung <showong24610@yahoo.com.tw>
2016-08-20 11:35:51 +0800

    資策會上課

--- Mars/src/tw/org/iii/Mars04.java
+++ Mars/src/tw/org/iii/Mars04.java
@@ -3,24 +3,26 @@ package tw.org.iii;
 public class Mars04 {
 
 	public static void main(String[] args) {
-		char a=128;
+		char a=10;
 		final  int b=9;
-		switch(b){
+		int c=1;
+		switch(c){
+		default:
+			System.out.println("X");
+			break;
 		case 1:
 			System.out.println("A");
 			break;
 		case 10:
 			System.out.println("B");
-			break;
+			
 		case b:
 		    System.out.println("B2");
 		    break;
 		case 127:
 			System.out.println("C");
-			break;
-		default:
-			System.out.println("X");
-			break;
+		    break;
+		
 		}
 
 	}
