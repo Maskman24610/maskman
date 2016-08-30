f045010097c4e547c2082c1cb27d7f5c8150d13a
Hsu yung <showong24610@yahoo.com.tw>
2016-08-30 12:51:45 +0800

    資策會上課練習

--- Mars/src/tw/org/iii/Bike.java
+++ Mars/src/tw/org/iii/Bike.java
@@ -1,10 +1,15 @@
 package tw.org.iii;
 
 public class Bike {
-
-		// TODO Auto-generated method stub
-       private double speed;
-       void upSpeed(){
+	   private double speed;
+       static int count;
+       public Bike(){
+    	  count++;
+    	  speed=1;
+    	  System.out.println("Bike():"+speed);
+       }
+       
+       protected void upSpeed(){
     	   speed=speed<1?1:speed*1.2;   
        }
        void downSpeed(){
